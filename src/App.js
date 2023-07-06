f77744cc34aef341b2a53f64c4e26b9fb7d98c61
dctristero <123331986+dctristero@users.noreply.github.com>
2023-07-06 17:45:35 -0500

    react app install, some file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Portfolio from './pages/portfolio/portfolio';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio></Portfolio>
+    </div>
+  );
+}
+
+export default App;
