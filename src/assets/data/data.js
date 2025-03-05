abc7787cda7e50e03fc92ccddde639b4f909b83d
dctristero <123331986+dctristero@users.noreply.github.com>
2025-03-05 16:08:57 -0600

    new headshot, resume, portfolio thumbs, added ROS

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,57 +1,66 @@
 // image imports go here
 
-import dj from "../images/thumb-daja.jpg"
-import ots from "../images/thumb-ontheshelf.jpg"
-import placeholder from "../images/tumblr.png"
-import dc from "../images/DANNY.jpg"
-import tao from "../images/thumb-tao2.jpg"
+import dj from "../images/thumb-daja.jpg";
+import ots from "../images/thumb-ontheshelf.jpg";
+import placeholder from "../images/tumblr.png";
+import dc from "../images/DANNY.jpg";
+import tao from "../images/thumb-tao2.jpg";
+import ros from "../images/thumb-ros.jpg";
 
 export const bigProjCard = {
-   image: tao
-}
+  image: tao,
+};
 
 export const aboutPic = {
-   image: dc
-}
+  image: dc,
+};
 
 export const portfolioCards = [
-   {
-      id: 1,
-      title: "DJ Limavi",
-      description: "A webapp for finding music",
-      image: dj,
-      URL: "https://matthewmontiel.github.io/daja-limavi/",
-      alt: "a screenshot of On the Shelf",
-   },
-   {
-      id: 2,
-      title: "On the Shelf",
-      description: "A webapp for cataloging board games",
-      image: ots,
-      URL: "https://ontheshelf-b06e928cc0f2.herokuapp.com",
-      alt: "a screenshot of On the Shelf",
-   }
-]
+  {
+    id: 1,
+    title: "ROS concert programs",
+    description: "A template for responsive concert programs",
+    image: ros,
+    URL: "https://rossings.org/programs/timbuctoo/",
+    alt: "a screenshot of the Rochester Oratorio Society concert program",
+  },
+  {
+    id: 2,
+    title: "DJ Limavi",
+    description: "A webapp for finding music",
+    image: dj,
+    URL: "https://matthewmontiel.github.io/daja-limavi/",
+    alt: "a screenshot of DJ Limavi",
+  },
+  {
+    id: 3,
+    title: "On the Shelf",
+    description: "A webapp for cataloging board games",
+    image: ots,
+    URL: "https://ontheshelf-b06e928cc0f2.herokuapp.com",
+    alt: "a screenshot of On the Shelf",
+  },
+];
 
 export const proficiencyCards = [
-   {
-      id: 1,
-      title: "Languages",
-      body: "HTML5, CSS3, JavaScript ES6+, SQL",
-   },
-   {
-      id: 2,
-      title: "Applications",
-      body: "Git, GitHub, MongoDB, MySQL, Heroku",
-   },
-   {
-      id: 3,
-      title: "Tools",
-      body: " Express, React, Node, Bootstrap, Bulma, GraphQL, Mongoose, Jest",
-   },
-   {
-      id: 4,
-      title: "Other",
-      body: "professional teaching experience, professional writing experience",
-   }
-]
+  {
+    id: 1,
+    title: "Languages",
+    body: "HTML5, CSS3, JavaScript ES6+, SQL",
+  },
+  {
+    id: 2,
+    title: "Applications",
+    body: "Git, GitHub, MongoDB, MySQL, Heroku",
+  },
+  {
+    id: 3,
+    title: "Tools",
+    body: " Express, React, Node, Bootstrap, Bulma, GraphQL, Mongoose, Jest",
+  },
+  {
+    id: 4,
+    title: "Other",
+    body: "Wordpress, professional teaching experience, professional writing experience",
+  },
+];
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,70 +1,70 @@
 import React from "react";
 import { chakra, Box, Flex, Icon, Stack } from "@chakra-ui/react";
-import Footer from "../components/footer"
+import Footer from "../components/footer";
 
 const Contact = () => {
   return (
-   <>
-    <Flex
-      bg={["primary.100"]}
-      _dark={{ bg: "#3e3e3e" }}
-      p={7}
-      minHeight={{
-        base: "calc(100vh - 50px)",
-        sm: "calc(100vh - 60px)",
-        xl: "calc(100vh - 75px)",
-      }}
-      w="full"
-      justifyContent="center"
-      // alignItems="center"
-      // pos="absolute"
-    >
-      <Box
-        w="full"
-        h="full"
+    <>
+      <Flex
         bg={["primary.100"]}
-        _dark={{ bg: "gray.800" }}
-        px={0}
-        py={0}
-        mx="auto"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
+        _dark={{ bg: "#3e3e3e" }}
+        p={7}
+        minHeight={{
+          base: "calc(100vh - 50px)",
+          sm: "calc(100vh - 60px)",
+          xl: "calc(100vh - 75px)",
+        }}
+        w="full"
+        justifyContent="center"
+        // alignItems="center"
+        // pos="absolute"
       >
         <Box
-          // pos="fixed"
-          bg={["primary.200"]}
-          rounded="lg"
-          pt={{ base: 2, md: 0 }}
-          pb={{ base: 2, md: 1 }}
-          mt="2vw"
-          mb="4vw"
-          w={{ base: "80%", sm: "45%", lg:"25%", xl: "20%" }}
+          w="full"
+          h="full"
+          bg={["primary.100"]}
+          _dark={{ bg: "gray.800" }}
+          px={0}
+          py={0}
           mx="auto"
-          px={{ base: 2, lg: 0 }}
-          justifyContent="center"
+          display="flex"
+          flexDirection="column"
           alignItems="center"
         >
-          <chakra.h1
-            textAlign="center"
-            display="block"
-            color={["primary.300"]}
-            _dark={{ color: "white" }}
-            fontWeight="bold"
-            fontSize={{ base: "4xl", md: "5xl" }}
-            // fontSize="6xl"
-            mt={1}
+          <Box
+            // pos="fixed"
+            bg={["primary.200"]}
+            rounded="lg"
+            pt={{ base: 2, md: 0 }}
+            pb={{ base: 2, md: 1 }}
+            mt="2vw"
+            mb="4vw"
+            w={{ base: "80%", sm: "45%", lg: "25%", xl: "20%" }}
+            mx="auto"
+            px={{ base: 2, lg: 0 }}
+            justifyContent="center"
+            alignItems="center"
           >
-            Contact
-          </chakra.h1>
-        </Box>
+            <chakra.h1
+              textAlign="center"
+              display="block"
+              color={["primary.300"]}
+              _dark={{ color: "white" }}
+              fontWeight="bold"
+              fontSize={{ base: "4xl", md: "5xl" }}
+              // fontSize="6xl"
+              mt={1}
+            >
+              Contact
+            </chakra.h1>
+          </Box>
 
-        {/* "chakra ui grid examples" and play with justify css for text alignment, 
+          {/* "chakra ui grid examples" and play with justify css for text alignment, 
         use <br/> for line breaks */}
-        
-        {/* <Box maxW="6xl" mx="auto" px={{ base: 4, lg: 8 }}> */}
-        <Box
-            textAlign="center" 
+
+          {/* <Box maxW="6xl" mx="auto" px={{ base: 4, lg: 8 }}> */}
+          <Box
+            textAlign="center"
             bg={["primary.200"]}
             roundedBottom="lg"
             roundedTop="lg"
@@ -83,55 +83,53 @@ const Contact = () => {
               fontSize={{ base: "lg", sm: "xl", md: "2xl" }}
               mt={0}
             >
-               Email:&nbsp;             
-               <chakra.a
+              Email:&nbsp;
+              <chakra.a
                 href="mailto:dctristero@gmail.com"
                 target="_blank"
                 // rel="noreferrer"
                 color={["primary.300"]}
                 fontWeight="400"
-                _hover={{ color: "primary.400"}}
+                _hover={{ color: "primary.400" }}
               >
                 dctristero@gmail.com
               </chakra.a>
-               <br/>
-               <br/>
-               Phone: (512) 644-1087
-               <br/>
-               <br/>
-               GitHub:&nbsp; 
-               <chakra.a
+              <br />
+              <br />
+              Phone: (512) 644-1087
+              <br />
+              <br />
+              GitHub:&nbsp;
+              <chakra.a
                 href="https://github.com/dctristero"
                 target="_blank"
                 // rel="noreferrer"
                 color={["primary.300"]}
                 fontWeight="400"
-                _hover={{ color: "primary.400"}}
+                _hover={{ color: "primary.400" }}
               >
-               https://github.com/dctristero              
-               </chakra.a>
-               <br/>
-               <br/>
-               LinkedIn:&nbsp;
-               <chakra.a
+                https://github.com/dctristero
+              </chakra.a>
+              <br />
+              <br />
+              LinkedIn:&nbsp;
+              <chakra.a
                 href="https://www.linkedin.com/in/daniel-cohen-46aa31284/"
                 target="_blank"
                 // rel="noreferrer"
                 color={["primary.300"]}
                 fontWeight="400"
-                _hover={{ color: "primary.400"}}
+                _hover={{ color: "primary.400" }}
               >
-               Daniel Cohen              
-               </chakra.a>
+                Daniel Cohen
+              </chakra.a>
             </chakra.p>
           </Box>
-        {/* </Box> */}
-      </Box>
-      <Footer/>
-    </Flex>
+          {/* </Box> */}
+        </Box>
+        <Footer />
+      </Flex>
     </>
-   
   );
 };
 export default Contact;
-
